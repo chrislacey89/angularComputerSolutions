ae2147f2349eb4d9259c09b20b6fb4e291d44953
Christopher Lacey <christopherlacey@Chris-Mac.local>
2020-04-21 16:18:26 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,56 @@
+var app = angular
+  .module('computer', ['ngRoute'])
+
+  .config([
+    '$routeProvider',
+    function ($routeProvider) {
+      $routeProvider
+        .when('/main', {
+          templateUrl: 'main.html',
+          controller: 'MainCtrl',
+        })
+        .when('/about', {
+          templateUrl: 'about.html',
+          controller: 'MainCtrl',
+        })
+        .when('/services', {
+          templateUrl: 'main.html',
+          controller: 'ServicesCtrl',
+        })
+        .when('/contact', {
+          templateUrl: 'main.html',
+          controller: 'ContactCtrl',
+        })
+        .otherwise({ redirectTo: '/main' });
+    },
+  ])
+
+  .controller('MainCtrl', [
+    '$scope',
+    '$http',
+    function ($scope, $http) {
+      $http.get('services.json').then(function (response) {
+        $scope.services = response.data;
+      });
+    },
+  ])
+
+  .controller('ServicesCtrl', [
+    '$scope',
+    '$http',
+    function ($scope, $http) {
+      $http.get('services.json').then(function (response) {
+        $scope.services = response.data;
+      });
+    },
+  ])
+
+  .controller('ContactCtrl', [
+    '$scope',
+    '$http',
+    function ($scope, $http) {
+      $http.get('locations.json').then(function (response) {
+        $scope.locations = response.data;
+      });
+    },
+  ]);
